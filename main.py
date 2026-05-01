5a9f4832510b89eddcf920d02b6ec23f6686b48e
Joseph <jahar0930@gmail.com>
2026-04-30 21:03:10 -0500

    Made some things a bit easier to read and added more information in game about what is going on.

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
-#This is my attempt at working out the dueling mechanic for a game I'm working on. When an officer attacks the other,
-#a duel is triggered. The two officers battle each other until one is fully beaten. 
+#This is my attempt at working out the dueling mechanic for a game I'm working on. 
+#When an officer attacks the other, a duel is triggered. 
+#The two officers battle each other until one is fully beaten. 
 #Every officer will have 3 attacks. Thrust, Strike, and Smash. They will also have 3 defensive options. Parry, Block, and Dodge
+
 #A duel will work like Rock, Paper, Scissors.
 #Thrust and Dodge are Paper.
 #Strike and Parry are Scissors.
 #Smash and Block are Rock.
-#If the attacker loses the matchup, there is no damage. For instance, Thrust is countered by Parry.
-#If the defender loses the matchup, there is double damage. For instance, Thrust deals double damage vs Dodge
-#If neither win, the attacker deals normal damage, for instance, Thrust being used against Block.
+
+#Thrust beats Block, loses to Parry.
+#Strike beats Dodge, loses to Block.
+#Smash beats Parry, loses to Dodge.
+
 #The two officer exchange blows. The attacker in round 1 is the defender in round 2. 
 #Each player draws 1 of each type of card. 
 #The cards will have a power between 1 and 9. Higher is better.
+
 #Each officer will have a number assigned to each type of attack. For instance, Sir Frederick wields a mace.
 #He has 5 Smash, 3 Strike, and 1 Thrust as base damage.
 #If Sir Frederick uses a 9 power Smash card, his smash stat will be 14. 
 #This also means he should be using more Smash and Strike attacks than Thrust.
+
 #Defense card power will reduce the power of the attack, but only a successful matchup can negate all damage.
 #The minimum power of a successful attack is 1. If a 2 power attack is met by a 9 power defense, the damage will still be 1.
-import random
+#if the attaker wins the card matchup, the attacker deals a bonus 10 damage.
+
 import copy
 import roster
-from draw_card import draw_attack_cards, draw_defense_cards
 from turn import take_turn
 from input import get_input
 
@@ -34,6 +40,8 @@ def get_character(character):
             return copy.deepcopy(roster.char_kiln)
         case 4: 
             return copy.deepcopy(roster.char_sint_drage)
+        case 5:
+            return copy.deepcopy(roster.char_testudo_iratus)
     
 #do_battle should loop between attack and defense phases for the player and the computer until one of defeated, then return the conclusion of the battle.
 def do_battle(user, opp):
@@ -50,19 +58,19 @@ def do_battle(user, opp):
 
 def main():
     #Choosing a warrior returns a copy of the character, allowing mirror matches.
-    print("Choose your warrior!\n1. Kumayari, the Samurai\n2. Sir Frederick, the Knight\n3. Kiln, the Adventurer\n4. Sint Drage, the Dragon Warrior")
+    print("Choose your warrior!\n\n1. Kumayari, the Samurai\n2. Sir Frederick, the Knight\n3. Kiln, the Adventurer\n4. Sint Drage, the Dragon Warrior")
     player_choice = get_input(1, 4)
     player_character = get_character(player_choice)
     player_character.player = True
-    print(f"You have chosen {player_character.name}. Thrust {player_character.thrust}, Strike {player_character.strike}, Smash {player_character.smash}.")
+    print(f"You have chosen {player_character.name}. Thrust {player_character.thrust}, Strike {player_character.strike}, Smash {player_character.smash}.\n")
 
     print("Choose your opponent!\n1. Kumayari, the Samurai\n2. Sir Frederick, the Knight\n3. Kiln, the Adventurer\n4. Sint Drage, the Dragon Warrior")
     opponent_choice = get_input(1, 4)
     opponent_character = get_character(opponent_choice)
-    print(f"Your foe is {opponent_character.name}. Thrust {opponent_character.thrust}, Strike {opponent_character.strike}, Smash {opponent_character.smash}.")
+    print(f"Your foe is {opponent_character.name}. Thrust {opponent_character.thrust}, Strike {opponent_character.strike}, Smash {opponent_character.smash}.\n")
 
     #do_battle should loop until either the player or the computer character's health drops below 1, hen print 
-    print(f"The battle between {player_character.name} and {opponent_character.name} begins!")
+    print(f"{player_character.name} Versus {opponent_character.name}!\n")
     conclusion = do_battle(player_character, opponent_character)
     print(conclusion)
 main()
--- turn.py
+++ turn.py
@@ -7,14 +7,16 @@ def take_turn(attack, defense):
     attack.cards = draw_attack_cards()
     defense.cards = draw_defense_cards()
     if attack.player == True:
-        print(attack.cards)
+        print("Choose your attack!\n")
+        print(f"1. {attack.cards[0][0]} {attack.cards[0][1] + attack.thrust}\n2. {attack.cards[1][0]} {attack.cards[1][1] + attack.strike}\n3. {attack.cards[2][0]} {attack.cards[2][1] + attack.smash}")
         user_input = get_input(1 , 3)
         attack.cards = attack.cards[user_input - 1]
     else:
         attack.cards = opp_attack_logic(attack.cards)
 
     if defense.player == True:
-        print(defense.cards)
+        print("Choose your defense!\n")
+        print(f"1. {defense.cards[0][0]} {defense.cards[0][1]}\n2. {defense.cards[1][0]} {defense.cards[1][1]}\n3. {defense.cards[2][0]} {defense.cards[2][1]}")
         user_input = get_input(1 , 3)
         defense.cards = defense.cards[user_input - 1]
     else:
@@ -24,12 +26,13 @@ def take_turn(attack, defense):
 
 #do_attack matches the card types together, then works out what happens based on the power of the cards and the base attacking stat of the character.
 def do_attack(attacker, defender):
+    print(f"{attacker.name} attacks with a {attacker.cards[0]} of {attacker.cards[1]} power!\n{defender.name} answers with a {defender.cards[0]} of {defender.cards[1]} power!\n")
     damage = 0
     match attacker.cards[0]:
         case "Thrust":
             match defender.cards[0]:
                 case "Parry":
-                    print(f"{defender.name} successfully parries {attacker.name}'s thrust!")
+                    print(f"{defender.name} successfully parries {attacker.name}'s thrust!\n")
                 
                 case "Dodge":
                     if (attacker.thrust + attacker.cards[1]) - defender.cards[1] < 1:
@@ -41,12 +44,12 @@ def do_attack(attacker, defender):
                     print(f"{attacker.name} thrusts and stabs {defender.name} for {damage} damage!\n")
                 
                 case "Block":
-                    if (((attacker.thrust + attacker.cards[1]) - defender.cards[1]) *2) < 2:
-                        damage = 2
+                    if (((attacker.thrust + attacker.cards[1]) - defender.cards[1]) +10) < 10:
+                        damage = 10
                     else:
-                        damage = ((attacker.thrust + attacker.cards[1]) - defender.cards[1]) *2
+                        damage = ((attacker.thrust + attacker.cards[1]) - defender.cards[1]) +10
                     defender.health -= damage
-                    print(f"{attacker.name} thrusts past {defender.name}'s block and skewers him for {damage} damage!")
+                    print(f"{attacker.name} thrusts past {defender.name}'s block and skewers him for {damage} damage!\n")
         
         case "Strike":
             match defender.cards[0]:
@@ -59,21 +62,21 @@ def do_attack(attacker, defender):
                     else:
                         damage = (attacker.strike + attacker.cards[1]) - defender.cards[1]
                     defender.health -= damage
-                    print(f"{attacker.name} strikes {defender.name} for {damage} damage!")
+                    print(f"{attacker.name} strikes {defender.name} for {damage} damage!\n")
 
                 case "Dodge":
-                    if (((attacker.strike + attacker.cards[1]) - defender.cards[1]) *2) < 2:
+                    if (((attacker.strike + attacker.cards[1]) - defender.cards[1]) +10) < 10:
                         damage = 2
                     else:
-                        damage = ((attacker.strike + attacker.cards[1]) - defender.cards[1]) *2
+                        damage = ((attacker.strike + attacker.cards[1]) - defender.cards[1]) +10
 
                     defender.health -= damage
-                    print(f"{attacker.name} lands a mighty strike on {defender.name} for {damage} damage!")
+                    print(f"{attacker.name} lands a mighty strike on {defender.name} for {damage} damage!\n")
 
         case "Smash":
             match defender.cards[0]:
                 case "Dodge":
-                    print(f"{defender.name} successfully dodges {attacker.name}'s smash!")
+                    print(f"{defender.name} successfully dodges {attacker.name}'s smash!\n")
 
                 case "Block":
                     if (attacker.smash + attacker.cards[1]) - defender.cards[1] < 1:
@@ -82,16 +85,16 @@ def do_attack(attacker, defender):
                         damage = (attacker.smash + attacker.cards[1]) - defender.cards[1]
 
                     defender.health -= damage
-                    print(f"{attacker.name} smashes his weapon into {defender.name} for {damage} damage!")
+                    print(f"{attacker.name} smashes his weapon into {defender.name} for {damage} damage!\n")
 
                 case "Parry":
-                    if (((attacker.smash + attacker.cards[1]) - defender.cards[1]) *2) < 2:
-                        damage = 2
+                    if (((attacker.smash + attacker.cards[1]) - defender.cards[1]) + 10) < 10:
+                        damage = 10
                     else:
-                        damage = ((attacker.smash + attacker.cards[1]) - defender.cards[1]) * 2
+                        damage = ((attacker.smash + attacker.cards[1]) - defender.cards[1]) + 10
 
                     defender.health -= damage
-                    print(f"{attacker.name} smashes through {defender.name}'s parry for {damage} damage!")
+                    print(f"{attacker.name} smashes through {defender.name}'s parry for {damage} damage!\n")
 
 
 #I intend to work on these more later. Right now, I'm more interested in getting the rest of the program set up. 
